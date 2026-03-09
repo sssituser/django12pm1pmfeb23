fb54e6dac2e4d1afcf5d670b243e1d797bb8f222
arun <allamarunkumar@gmail.com>
2026-03-09 16:07:58 +0530

    modeproject added

---
+++ modelproject4/myapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from myapp.models import Employee
+# Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=["EmpId","EmpName","EmpSal"]
+    model = Employee
+admin.site.register(Employee,EmployeeAdmin)
+    
+    
---
+++ modelproject4/myapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from myapp.models import Employee
+# Create your views here.
+def getEmployees(request):
+    employees = Employee.objects.all()
+    return render(request,'myapp/employees.html',{"emps":employees})
